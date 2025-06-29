2a64e401f04b280d0878259876df3d11d773f065
docaohuynh <docaohuynh0601@gmail.com>
2025-06-29 09:52:02 +0700

    export function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { splitSentenceByLanguge } from './sentence/sentences.util'
 export type { LanguageModel } from './models/language.model'
 export { LanguageCode } from './models/language.model'
-export { splitByParagraph, splitTextByParagraphAndSentenceWithSpacing } from './splits/paragraph-split.util'
+export { splitByParagraph, splitTextByNewLine, splitTextByParagraphAndSentenceWithSpacing } from './splits/paragraph-split.util'
